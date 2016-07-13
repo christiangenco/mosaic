547182fd34f8ab4a7ce13fa160cb52421e627f37
Christian Genco <christian.genco@gmail.com>
2016-07-13 16:05:39 -0500

    simplify logic

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -1,45 +1,38 @@
 var cloudinaryURL = "https://api.cloudinary.com/v1_1/" + cloudinary.cloud_name + "/image/upload";
 
-function getFileBytes(file){
-  return new Promise(function(fulfill, reject){
-    reader = new FileReader()
-    reader.onload = function(event) {
-      var arrayBuffer = reader.result
-      var bytes = new Uint8Array(arrayBuffer);
-      fulfill(bytes);
-    };
-    reader.readAsArrayBuffer(file);
-  });
-}
-
 window.imageupload = function(file){
   return new Promise(function (resolve, reject){
-    getFileBytes(file).then(function(bytes){
-      var data = new FormData();
-      data.append('upload_preset', cloudinary.upload_preset);
-      data.append('api_key', cloudinary.api_key);
-      data.append('timestamp', Date.now() / 1000 | 0);
-      data.append("file", file);
+    var data = new FormData();
+    data.append('upload_preset', cloudinary.upload_preset);
+    // data.append('api_key', cloudinary.api_key);
+    // data.append('timestamp', Date.now() / 1000 | 0);
+    data.append("file", file);
 
-      var request = new XMLHttpRequest();
-      request.open('POST', cloudinaryURL, true);
-      // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-      request.setRequestHeader("Accept", "application/json");
-      request.setRequestHeader("Cache-Control", "no-cache");
-      request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-      request.send(data);
+    var request = new XMLHttpRequest();
+    request.open('POST', cloudinaryURL, true);
+    // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.setRequestHeader("Accept", "application/json");
+    request.setRequestHeader("Cache-Control", "no-cache");
+    request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
-      request.onload = function() {
-        console.dir(request);
-        console.info(request.status)
-        if (request.status >= 200 && request.status < 400) {
-          // Success!
-          var resp = request.responseText;
-        } else {
-          // We reached our target server, but it returned an error
+    request.onload = function(){
+      console.dir(request);
+      console.info(request.status)
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var resp = request.responseText;
+      } else {
+        // We reached our target server, but it returned an error
 
-        }
-      };
-    })
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    // request.onProgress = function(){}
+
+    request.send(data);
   });
 }
